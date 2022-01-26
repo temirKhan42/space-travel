c565dc3da10ddf7fba8dfd148547a70f083c9029
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2022-01-27 03:10:06 +0600

    Rename file

---
+++ src/init.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import '../styles/main.scss';
+
+export default () => {
+  ReactDOM.render(<App />, document.getElementById('root'));
+};
