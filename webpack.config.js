788921bc774d8b8b4315e485fb2f7b9c07602d7b
temirKhan42 <52080219+temirKhan42@users.noreply.github.com>
2022-01-27 03:05:00 +0600

    Mod configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
   mode,
-  entry: "./index.js",
-	output: {
-		filename: "main.[fullhash].js",
-		path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "./imgs/[name].[fullhash].[ext]",
-	},
+  entry: './index.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: 'main.[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: './imgs/[name].[fullhash].[ext]',
+  },
   module: {
     rules: [
       {
@@ -22,21 +25,21 @@ module.exports = {
       },
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ['html-loader'],
       },
       {
         test: /\.s[ac]ss$/,
-        use: [                             
-          MiniCssExtractPlugin.loader,    
-          "css-loader",
-          "sass-loader"
-        ]
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
       },
-    ]
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({ template: './public/index.html' }),
     new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
     new CleanWebpackPlugin(),
   ],
-}
+};
